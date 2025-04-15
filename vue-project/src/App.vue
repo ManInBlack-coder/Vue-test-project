7f55fba428192f864c3f7a4239e4a16b67327808
Aleksander P <77441538+ManInBlack-coder@users.noreply.github.com>
2025-04-15 14:25:44 +0300

    created first NUXT/VUE3/VITE project

---
+++ vue-project/src/App.vue
@@ -0,0 +1,24 @@
+import component_one from './components/component_one.vue'
+
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    Test app
+    
+  </div>
+  <component_one/>
+
+<div>
+</div>
+
+
+</template>
+
+<style scoped>
+
+
+
+</style>
---
+++ vue-project/src/components/component_one.vue
@@ -0,0 +1,16 @@
+
+
+
+<template>
+ 
+
+    <div>
+        <textarea name="" id="">This is sample text from another component</textarea>
+    </div>
+</template>
+
+
+<script>
+
+ console.log('component_one');
+</script> 
